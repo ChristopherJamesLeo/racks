f7488e0729b4c08e4a25b1998f9b0d9437a12001
ChristopherJamesLeo <aungaungoo.hotmail@gmail.com>
2023-06-23 14:34:32 +0630

    add index page

--- js/index.js
+++ js/index.js
@@ -1,25 +1,59 @@
 
 // Start Jquery Area
-
+$(window).on("load",function(){
+    $("#preloader").delay(300).fadeOut(200);
+})
 $(document).ready(function(){
     console.log("hello jquery");
-    $("#banner_carousel").owlCarousel({
-        items : 1,
-        loop : true,
-    });
-
+    // start nav bar section
     $(window).scroll(function(){
-        let getscrollcount = $(this).scrollTop();
-        // console.log(getscrollcount);
-        if(getscrollcount > 400){
+        let getscrollval = $(this).scrollTop();
+        if(getscrollval > 500){
             $(".navbar").addClass("active");
         }else{
             $(".navbar").removeClass("active");
         }
     })
+    // end nav bar section
+    // start banner carousel
+    $("#banner_carousel").owlCarousel({
+        items : 1,
+        loop : true,
+    });
+
+    // end banner carousel
+
+    // start customer say section
+    $("#customer_say_carousel").owlCarousel({
+        items : 3,
+        loop : true,
+        nav : true,
+        navText : ["<span class='left'><i class='fas fa-angle-left'></i></span>","<span class='right'><i class='fas fa-angle-right'></i></span>"],
+        responsive : {
+            0 : {
+                items : 1
+            },
+            465 : {
+                items : 1
+            },
+            768 : {
+                items : 2
+            },
+            992 :{
+                items : 3
+            }
+        },
+
+    })
+
+    // end customer say section
+
+    
 })
 
 // End Jquery Area
+
+// start banner text effect
 
 let gettitles = ["Capture.","Design.","Explore.","Develop.","Graphic."];
 
@@ -85,4 +119,36 @@ function deletetitle(){
 
 }
 
+// end banner text effect
 
+// start achievement section
+
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 5000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const  satisfied = document.querySelector( '#satisfied' )
+const  amazing = document.querySelector( '#amazing' )
+const  response = document.querySelector( '#response' )
+IO.observe( satisfied );
+IO.observe( amazing );
+IO.observe( response );
+
+// end achievement section
+
+// start wow js 
+new WOW().init();
+// end wow js
